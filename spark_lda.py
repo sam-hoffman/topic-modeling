4d1e84f0f061d3ea0c12080ae57871b3b0bb8a4f
Sam Hoffman <sam.hoffman99@gmail.com>
2020-07-21 05:40:45 +0000

    spark topic modelling runs and is a lot of fun :)

--- spark_lda.py
+++ spark_lda.py
@@ -1,23 +1,27 @@
 from pyspark.ml.feature import CountVectorizer
 from pyspark.ml.clustering import LDA
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import udf, split, monotonically_increasing_id
+from pyspark.sql.functions import udf, min as min_,  split, monotonically_increasing_id, slice as slice_, to_date, max as max_
 from pyspark.sql.types import StringType, ArrayType
+import os
 
 # see https://stackoverflow.com/questions/42051184/latent-dirichlet-allocation-lda-in-spark
 
+print(os.getcwd())
 spark = SparkSession.builder.config("spark.worker.cleanup.enabled", "true") .config("spark.worker.cleanup.interval", 60) .getOrCreate() 
-df = spark.read.json("cleaned_tweets")
-df = df.sample(0.05)
+df = spark.read.json("/user/hadoop/cleaned_tweets/")
+# df = df.sample(0.5)
+df = df.withColumn("dt", to_date("created_at", "EEE MMM dd HH:mm:ss +SSSS yyy")) 
 df = df.withColumn("id", monotonically_increasing_id())
 df = df.withColumn("split_text", split(df.cleaned_text, " "))
-cv = CountVectorizer(inputCol="split_text", outputCol="raw_features", vocabSize=2**18, maxDF=0.2)
+cv = CountVectorizer(inputCol="split_text", outputCol="raw_features") 
 cvmodel = cv.fit(df)
 vocab = cvmodel.vocabulary
 result_cv = cvmodel.transform(df)
-lda = LDA(featuresCol = "raw_features", k=50, seed=1, optimizer="em")
+# online optimizer is critical, algorithm doesn't converge with em optimizer :(
+lda = LDA(featuresCol = "raw_features", k=5, optimizer="online")
 lda_model = lda.fit(dataset=result_cv)
-num_words_per_topic = 20
+num_words_per_topic = 50
 # default is 10 terms per topic
 described = lda_model.describeTopics(maxTermsPerTopic=num_words_per_topic)
 
@@ -31,8 +35,6 @@ def topic_render(topic):
 topic_render_udf = udf(topic_render, ArrayType(StringType()))
 
 described = described.withColumn("translated", topic_render_udf(described.termIndices))
-described.toPandas().to_json("lda_results.json", orient="records")
+# described.repartition(1).write.mode("overwrite").json("/user/hadoop/results")
 
-
-
-
+described.select("translated").show()
