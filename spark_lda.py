6d2e4fca1035c52e8e87bfbadee25c4c98a640ff
Sam Hoffman <sam.hoffman99@gmail.com>
2020-07-23 05:54:13 +0000

    added some udfs everyone loves udfs

--- spark_lda.py
+++ spark_lda.py
@@ -1,8 +1,8 @@
 from pyspark.ml.feature import CountVectorizer
 from pyspark.ml.clustering import LDA
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import udf, min as min_,  split, monotonically_increasing_id, slice as slice_, to_date, max as max_
-from pyspark.sql.types import StringType, ArrayType
+from pyspark.sql.functions import udf, min as min_,  split, monotonically_increasing_id, slice as slice_, to_date, max as max_, array_position, array_max, expr, element_at
+from pyspark.sql.types import StringType, ArrayType, IntegerType, FloatType
 import os
 
 # see https://stackoverflow.com/questions/42051184/latent-dirichlet-allocation-lda-in-spark
@@ -19,7 +19,7 @@ cvmodel = cv.fit(df)
 vocab = cvmodel.vocabulary
 result_cv = cvmodel.transform(df)
 # online optimizer is critical, algorithm doesn't converge with em optimizer :(
-lda = LDA(featuresCol = "raw_features", k=5, optimizer="online")
+lda = LDA(featuresCol = "raw_features", k=50, optimizer="online")
 lda_model = lda.fit(dataset=result_cv)
 num_words_per_topic = 50
 # default is 10 terms per topic
@@ -35,7 +35,21 @@ def topic_render(topic):
 topic_render_udf = udf(topic_render, ArrayType(StringType()))
 
 described = described.withColumn("translated", topic_render_udf(described.termIndices))
-described.repartition(1).write.mode("overwrite").json("s3://covid-tweets/model-summary")
+described.repartition(1).write.mode("overwrite").json("s3://covid-tweets/model-summary50")
 
 fit = lda_model.transform(result_cv)
-fit.write.mode("overwrite").json("s3://covid-tweets/fit-tweets")
+
+def argmax(v):
+    return int(v.argmax()) + 1
+argmax_udf = udf(argmax, IntegerType()) 
+
+fit.printSchema()
+fit = fit.withColumn("topTopic", argmax_udf("topicDistribution"))
+def arraymaker(v):
+    return list([float(x) for x in v])
+arraymaker_udf = udf(arraymaker, ArrayType(FloatType()))
+fit = fit.withColumn("arrayTopics", arraymaker_udf("topicDistribution"))
+fit = fit.withColumn("topTopicScore", array_max("arrayTopics"))
+print(fit.select("topTopic", "arrayTopics", "topTopicScore").head(5))
+fit.write.mode("overwrite").json("s3://covid-tweets/fit-tweets50")
+
