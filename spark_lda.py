deaf005712ef83343725728ae10a4ff4b44402ab
Sam Hoffman <sam.hoffman99@gmail.com>
2020-07-30 05:30:55 +0000

    redo top topics

--- spark_lda.py
+++ spark_lda.py
@@ -1,9 +1,10 @@
 from pyspark.ml.feature import CountVectorizer
 from pyspark.ml.clustering import LDA
-from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql.functions import udf, min as min_,  split, monotonically_increasing_id, slice as slice_, to_date, max as max_, array_position, array_max, expr, element_at
 from pyspark.sql.types import StringType, ArrayType, IntegerType, FloatType
 import os
+from functools import reduce
 
 # see https://stackoverflow.com/questions/42051184/latent-dirichlet-allocation-lda-in-spark
 
@@ -12,6 +13,7 @@ spark = SparkSession.builder.config("spark.worker.cleanup.enabled", "true") .con
 df = spark.read.json("s3://covid-tweets/cleaned-tweets")
 # df = df.sample(0.05)
 df = df.withColumn("dt", to_date("created_at", "EEE MMM dd HH:mm:ss +SSSS yyy")) 
+
 df = df.withColumn("id", monotonically_increasing_id())
 df = df.withColumn("split_text", split(df.cleaned_text, " "))
 cv = CountVectorizer(inputCol="split_text", outputCol="raw_features") 
@@ -19,8 +21,10 @@ cvmodel = cv.fit(df)
 vocab = cvmodel.vocabulary
 result_cv = cvmodel.transform(df)
 # online optimizer is critical, algorithm doesn't converge with em optimizer :(
-lda = LDA(featuresCol = "raw_features", k=50, optimizer="online")
-lda_model = lda.fit(dataset=result_cv)
+num_topics = 50
+lda = LDA(featuresCol = "raw_features", k=num_topics, optimizer="online")
+training_df = result_cv.where(result_cv.rt_indicator == 0)
+lda_model = lda.fit(dataset=training_df)
 num_words_per_topic = 50
 # default is 10 terms per topic
 described = lda_model.describeTopics(maxTermsPerTopic=num_words_per_topic)
@@ -35,7 +39,7 @@ def topic_render(topic):
 topic_render_udf = udf(topic_render, ArrayType(StringType()))
 
 described = described.withColumn("translated", topic_render_udf(described.termIndices))
-described.repartition(1).write.mode("overwrite").json("s3://covid-tweets/model-summary50")
+described.repartition(1).write.mode("overwrite").json("s3://covid-tweets/model-summary" + str(num_topics))
 
 fit = lda_model.transform(result_cv)
 
@@ -50,6 +54,15 @@ def arraymaker(v):
 arraymaker_udf = udf(arraymaker, ArrayType(FloatType()))
 fit = fit.withColumn("arrayTopics", arraymaker_udf("topicDistribution"))
 fit = fit.withColumn("topTopicScore", array_max("arrayTopics"))
-print(fit.select("topTopic", "arrayTopics", "topTopicScore").head(5))
-fit.write.mode("overwrite").json("s3://covid-tweets/fit-tweets50")
+fit.write.mode("overwrite").json("s3://covid-tweets/fit-tweets" + str(num_topics))
 
+# use window function instead a la 
+# https://stackoverflow.com/questions/38397796/retrieve-top-n-in-each-group-of-a-dataframe-in-pyspark
+dfs = []
+for i in range(num_topics):
+   top_topic_df = fit.where(fit.topTopic == i + 1)
+   top_topic_df = top_topic_df.sort("topTopicScore", ascending=False)
+   dfs.append(top_topic_df.limit(20))
+
+df_complete = reduce(DataFrame.unionAll, dfs)
+df_complete.repartition(1).write.mode("overwrite").json("s3://covid-tweets/top-tweets" + str(num_topics))
